49c2bab1a07c8a7bb3a03ffd7c0d2d19c9c28628
Richard Ewool <117762699+rcewool@users.noreply.github.com>
2025-04-25 10:40:12 -0500

    Update Brew_git_Test.R
    Update code with histogram plot information

--- Brew_git_Test.R
+++ Brew_git_Test.R
@@ -1,2 +1,17 @@
 # Testing Git and GitHub Config
 library(tidyverse)
+
+# Generate random sample data
+set.seed(123)  # for reproducibility
+
+data <- rnorm(1000, mean = 50, sd = 10)  # 1000 normally distributed numbers
+
+# Plot a histogram
+hist.defaut(data,
+     main = "Histogram of Normally Distributed Data",
+     xlab = "Value",
+     ylab = "Frequency",
+     col = "skyblue",
+     border = "white")
+
+# This is Just a Test 
